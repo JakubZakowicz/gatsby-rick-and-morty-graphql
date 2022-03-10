e7dd9461a3422e2c87212f6ab8d5289ddf999c60
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-03-10 21:57:32 +0100

    feat: add location scroll pagination

--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { Box, Container, Flex, Spacer, Text } from '@chakra-ui/react';
+import { BottomScrollListener } from 'react-bottom-scroll-listener';
 import Loader from '../components/Loader';
 import DetailsButton from '../components/DetailsButton';
 
 const GET_LOCATIONS = gql`
-  query getLocations {
-    locations {
+  query getLocations($page: Int) {
+    locations(page: $page) {
+      info {
+        next
+      }
       results {
         id
         name
@@ -16,30 +20,50 @@ const GET_LOCATIONS = gql`
 `;
 
 const Locations = () => {
-  const { data, loading } = useQuery(GET_LOCATIONS);
+  const { data, loading, fetchMore } = useQuery(GET_LOCATIONS);
 
   if (loading) return <Loader />;
 
   return (
     <Container mt="10">
-      {data?.locations?.results?.map((location, index) => (
-        <Box
-          mt="5"
-          bg="gray"
-          w="741px"
-          p="5"
-          borderRadius="15"
-          boxShadow="dark-lg"
-        >
-          <Flex align="center">
-            <Text fontSize="lg">{index + 1}.</Text>
-            <Spacer />
-            <Text fontSize="lg">{location.name}</Text>
-            <Spacer />
-            <DetailsButton name="See More" to={`/locations/${location.id}`} />
-          </Flex>
-        </Box>
-      ))}
+      <BottomScrollListener
+        onBottom={() => {
+          const { next } = data.locations.info;
+          if (next !== null)
+            fetchMore({
+              variables: { page: next },
+              updateQuery: (prevResult, { fetchMoreResult }) => ({
+                locations: {
+                  ...fetchMoreResult.locations,
+                  results: [
+                    ...prevResult.locations.results,
+                    ...fetchMoreResult.locations.results,
+                  ],
+                },
+              }),
+            });
+        }}
+      >
+        {data?.locations?.results?.map((location, index) => (
+          <Box
+            key={location.id}
+            mt="5"
+            bg="gray"
+            w="741px"
+            p="5"
+            borderRadius="15"
+            boxShadow="dark-lg"
+          >
+            <Flex align="center">
+              <Text fontSize="lg">{index + 1}.</Text>
+              <Spacer />
+              <Text fontSize="lg">{location.name}</Text>
+              <Spacer />
+              <DetailsButton name="See More" to={`/locations/${location.id}`} />
+            </Flex>
+          </Box>
+        ))}
+      </BottomScrollListener>
     </Container>
   );
 };
