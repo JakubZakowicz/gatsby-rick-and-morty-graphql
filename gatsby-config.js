4f6e53eb46bb4697de5d3a6c1f3b341dea4a4807
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-06-22 19:30:17 +0200

    feat: add static index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+const path = require(`path`);
+
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   plugins: [
     '@chakra-ui/gatsby-plugin',
     'gatsby-plugin-use-query-params',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-source-graphql',
       options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
-import { navigate } from 'gatsby';
 import React from 'react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
-const index = () => {
-  navigate('characters/1');
-  return <div>index</div>;
-};
+export const BackgroundQuery = graphql`
+  query BackgroundQuery {
+    file {
+      childImageSharp {
+        fixed(width: 1833) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+  }
+`;
 
-export default index;
+const Home = ({ data }) => (
+  <Box>
+    <Box
+      position="absolute"
+      top="0"
+      w="99vw"
+      h="100vh"
+      filter="auto"
+      brightness="40%"
+    >
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </Box>
+    <Flex
+      position="absolute"
+      top="0"
+      w="100%"
+      h="100%"
+      justify="center"
+      align="center"
+      direction="column"
+    >
+      <Text fontSize="6xl" fontWeight="semibold" textAlign="center">
+        Welcome to my Rick and Morty website
+      </Text>
+      <Button
+        as={Link}
+        to="/characters/1"
+        bg="yellow"
+        p="30px 100px"
+        fontSize="xl"
+        mt="8"
+        borderRadius="20px"
+      >
+        See more here
+      </Button>
+    </Flex>
+  </Box>
+);
+export default Home;
